16c04ad9903af9d75fcb0736f301cac57ae8eb10
Zak Henry <zak.henry@gmail.com>
2015-06-13 21:58:44 +1000

    Added multiline commit with more details

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -8,4 +8,14 @@ git init
 git remote add origin "git@github.com:xiphiaz/xiphiaz.github.io.git"
 git add --all
 git commit -am "TRAVIS_COMMIT: $TRAVIS_COMMIT"
+
+git commit -F- <<EOF
+Automated Travis CI Deployment: https://travis-ci.org/xiphiaz/zakhenry.com/builds/$TRAVIS_BUILD_ID
+
+Branch: $TRAVIS_BRANCH
+Build ID: $TRAVIS_BUILD_ID
+Build Number: $TRAVIS_BUILD_NUMBER
+Commit Hash: $TRAVIS_COMMIT
+EOF
+
 git push origin master -f
