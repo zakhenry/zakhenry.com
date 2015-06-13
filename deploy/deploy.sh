77d4df50d8328f04639a5f9ee976f7382beb0d9c
Zak Henry <zak.henry@gmail.com>
2015-06-13 19:22:29 +1000

    Fixed remote to use GH_TOKEN

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,7 +5,7 @@ cd build
 git config --global user.name "$GIT_NAME"
 git config --global  user.email "$GIT_EMAIL"
 git init
-git remote add origin git@github.com:xiphiaz/xiphiaz.github.io.git
+git remote add origin "$GH_TOKEN@github.com:xiphiaz/xiphiaz.github.io.git"
 git add --all
 git commit -am "TRAVIS_COMMIT: $TRAVIS_COMMIT"
 git push origin master -f
